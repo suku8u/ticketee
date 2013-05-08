ac91e8415921b555721558c44e18d6fb7c5640dc
Sukumar Ponram <suku8u@uw.edu>
2013-05-07 17:25:17 -0700

    Creation of projects feature complete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,8 +10,30 @@ class ProjectsController < ApplicationController
 
 	end
 
-	def create
-
-	end
-	
+def create
+  @project = Project.new(params[:project])
+  if @project.save
+    flash[:notice] = "Project has been created."
+    redirect_to @project
+  else
+    # nothing, yet
+  end
 end
+
+def show 
+	@project = Project.find(params[:id])
+end
+
+end
+
+#begin
+#{
+ # "commit"     => "Create Project",
+ # "action"     => "create",
+ # "project"    => {
+ #   "name" => "TextMate 2",
+ #   "description" => "A text-editor for OS X"
+ # },
+ # "controller" => "projects"
+#}
+ #emd
